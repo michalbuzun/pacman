3a52e77103c7e310afd03d0cacd8ab5c67d2c59b
michalbuzun <michal.buzun@doktor.se>
2024-07-01 21:31:19 +0200

    Randomize ghost movement

--- obstacle.py
+++ obstacle.py
@@ -2,6 +2,7 @@ import pygame
 from const import WIDTH, HEIGHT, POSSIBLE_MOVES
 import random
 from enum import Enum, auto
+from collections import namedtuple
 
 
 class Direction(Enum):
@@ -9,6 +10,15 @@ class Direction(Enum):
     RIGHT = auto()
     UP = auto()
     DOWN = auto()
+
+
+Position = namedtuple(
+    "Position",
+    [
+        "direction",
+        "position",
+    ],
+)
 
 
 class Obstacle(pygame.sprite.Sprite):
@@ -26,19 +36,155 @@ class Obstacle(pygame.sprite.Sprite):
 
     def _move(self):
         if self.direction == Direction.RIGHT:
-            potential_right_field = (self.rect.topleft[0] + 50, self.rect.topleft[1])
+            potential_position = (self.rect.topleft[0] + 50, self.rect.topleft[1])
 
-            if self._is_move_possible(potential_right_field):
-                self.rect.topleft = potential_right_field
+            if self._is_move_possible(potential_position):
+                possible_moves = self._get_possible_moves_in_ninty_degree_direction(
+                    self.rect.topleft
+                )
+                possible_moves_positions = [move.position for move in possible_moves]
+
+                all_possible_moves = [
+                    *possible_moves_positions,
+                    potential_position,
+                ]
+
+                self.rect.topleft = random.choice(all_possible_moves)
+
                 # TO DO: refine this movement by checking if there are other directions possible
                 # change direction if possible but with low probability for example 1/4 changing
                 # direction 4/4 going straight
             else:
-                ...
-                # next move algorithm for ghost:
-                # 1. check available directions
-                # 2. randomly choose a direction
-                # 3. go to the end in that direction
+                self._check_available_directions(self.rect.topleft)
+
+        if self.direction == Direction.LEFT:
+            potential_position = (self.rect.topleft[0] - 50, self.rect.topleft[1])
+
+            if self._is_move_possible(potential_position):
+                possible_moves = self._get_possible_moves_in_ninty_degree_direction(
+                    self.rect.topleft
+                )
+                possible_moves_positions = [move.position for move in possible_moves]
+
+                all_possible_moves = [
+                    *possible_moves_positions,
+                    potential_position,
+                ]
+
+                self.rect.topleft = random.choice(all_possible_moves)
+            else:
+                self._check_available_directions(self.rect.topleft)
+
+        if self.direction == Direction.UP:
+            potential_position = (self.rect.topleft[0], self.rect.topleft[1] - 50)
+
+            if self._is_move_possible(potential_position):
+                possible_moves = self._get_possible_moves_in_ninty_degree_direction(
+                    self.rect.topleft
+                )
+                possible_moves_positions = [move.position for move in possible_moves]
+
+                all_possible_moves = [
+                    *possible_moves_positions,
+                    potential_position,
+                ]
+
+                self.rect.topleft = random.choice(all_possible_moves)
+            else:
+                self._check_available_directions(self.rect.topleft)
+
+        if self.direction == Direction.DOWN:
+            potential_position = (self.rect.topleft[0], self.rect.topleft[1] + 50)
+
+            if self._is_move_possible(potential_position):
+                possible_moves = self._get_possible_moves_in_ninty_degree_direction(
+                    self.rect.topleft
+                )
+                possible_moves_positions = [move.position for move in possible_moves]
+
+                all_possible_moves = [
+                    *possible_moves_positions,
+                    potential_position,
+                ]
+
+                self.rect.topleft = random.choice(all_possible_moves)
+            else:
+                self._check_available_directions(self.rect.topleft)
+
+    def _get_possible_moves_in_ninty_degree_direction(self, position):
+        available_directions = []
+
+        if self.direction == Direction.UP or self.direction == Direction.DOWN:
+            # check to the right
+            potential_move = (position[0] + 50, position[1])
+            if self._is_move_possible(potential_move):
+                available_directions.append(
+                    Position(direction=Direction.RIGHT, position=potential_move)
+                )
+
+            # check to the left
+            potential_move = (position[0] - 50, position[1])
+            if self._is_move_possible(potential_move):
+                available_directions.append(
+                    Position(direction=Direction.LEFT, position=potential_move)
+                )
+
+        if self.direction == Direction.LEFT or self.direction == Direction.RIGHT:
+            # check to the top
+            potential_move = (position[0], position[1] - 50)
+            if self._is_move_possible(potential_move):
+                available_directions.append(
+                    Position(direction=Direction.UP, position=potential_move)
+                )
+
+            # check to the bottom
+            potential_move = (position[0], position[1] + 50)
+            if self._is_move_possible(potential_move):
+                available_directions.append(
+                    Position(direction=Direction.DOWN, position=potential_move)
+                )
+
+        return available_directions
+
+    def _check_available_directions(self, position):
+        # TO DO:
+        # subtract 50 in all directions and chek if is in possible fields
+        # remember direction of that field from current field, because it needs to be set as direciton
+
+        available_directions = []
+
+        # check to the right
+        potential_move = (position[0] + 50, position[1])
+        if self._is_move_possible(potential_move):
+            available_directions.append(
+                Position(direction=Direction.RIGHT, position=potential_move)
+            )
+
+        # check to the left
+        potential_move = (position[0] - 50, position[1])
+        if self._is_move_possible(potential_move):
+            available_directions.append(
+                Position(direction=Direction.LEFT, position=potential_move)
+            )
+
+        # check to the top
+        potential_move = (position[0], position[1] - 50)
+        if self._is_move_possible(potential_move):
+            available_directions.append(
+                Position(direction=Direction.UP, position=potential_move)
+            )
+
+        # check to the bottom
+        potential_move = (position[0], position[1] + 50)
+        if self._is_move_possible(potential_move):
+            available_directions.append(
+                Position(direction=Direction.DOWN, position=potential_move)
+            )
+
+        next_move = random.choice(available_directions)
+
+        self.rect.topleft = next_move.position
+        self.direction = next_move.direction
 
     def _is_move_possible(self, position):
         return self._is_in_game_field(position) and self._is_in_possible_moves(position)
